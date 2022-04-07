3f62c8ed4e6a98400d400253acf70219dbfef584
Ricardo Antunes <me@riscadoa.com>
2022-04-07 11:44:38 +0100

    Add clap dependency and argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use clap::Parser;
+
 mod data;
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// The path to the file to be compiled.
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+    
+    /// The format used to print the resulting turing machine.
+    #[clap(short, long)]
+    format: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Cli::parse();
+    // TODO
 }
