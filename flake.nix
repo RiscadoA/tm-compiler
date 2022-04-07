904a76e068b63e6b64707ab3e28a9188a67588dd
Ricardo Antunes <me@riscadoa.com>
2022-04-07 11:23:24 +0100

    It's all downhill from here

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem ( system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            pkgconfig
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            rust-bin.stable.latest.rust-analysis
+            rust-bin.stable.latest.rls
+          ];
+
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      }
+    );
+}
