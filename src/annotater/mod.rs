0798177dd7d4c6e07c41e7dadda226f260ce7332
Ricardo Antunes <me@riscadoa.com>
2022-04-21 16:27:41 +0100

    Add borrow checker annotater stage

--- src/annotater/mod.rs
+++ src/annotater/mod.rs
@@ -1,8 +1,10 @@
 use crate::data::{Exp, TokenLoc};
 
+mod borrow_checker;
 mod type_checker;
 
 pub fn annotate(exp: Exp<TokenLoc>) -> Result<Exp<type_checker::Annot>, String> {
     let ast = type_checker::type_check(exp)?;
+    borrow_checker::borrow_check(&ast)?;
     Ok(ast)
 }
