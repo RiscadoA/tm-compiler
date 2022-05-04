35139f11438b22732611a949920a175efec29e9b
Ricardo Antunes <me@riscadoa.com>
2022-05-04 15:32:46 +0100

    Restructure main function - Add CLI options for debug information - Add standard library loader macro

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 
 use std::collections::HashMap;
+use std::io::Read;
 
 mod annotater;
 mod data;
@@ -8,43 +9,143 @@ mod lexer;
 mod parser;
 mod simplifier;
 
+macro_rules! load_lib {
+    ($a:expr) => {
+        {
+            let mut lib = HashMap::new();
+            lib.insert($a.to_owned(), include_str!($a).to_owned());
+            lib
+        }
+    };
+
+    ($a:expr, $b:expr) => {
+        {
+            let mut lib = load_lib!($a);
+            lib.insert($b.to_owned(), include_str!($b).to_owned());
+            lib
+        }
+    };
+
+    ($a:expr, $($b:tt)*) => {
+        {
+            let mut lib = load_lib!($($b)*);
+            lib.insert($a.to_owned(), include_str!($a).to_owned());
+            lib
+        }
+    }
+}
+
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
+#[clap(group(
+    ArgGroup::new("input")
+        .required(true)
+        .args(&["path", "stdin"]),
+))]
 struct Cli {
     /// The path to the file to be compiled.
     #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
+    path: Option<std::path::PathBuf>,
     /// The format used to print the resulting turing machine.
     #[clap(short, long)]
     format: Option<String>,
+
+    /// Should the input be read from stdin instead of a file?
+    #[clap(short = 'i', long)]
+    stdin: bool,
+
+    /// Should the tokens be printed?
+    #[clap(short, long)]
+    tokens: bool,
+    /// Should the AST be printed?
+    #[clap(short, long)]
+    parser: bool,
+    /// Should the annotated AST be printed?
+    #[clap(short, long)]
+    annotated: bool,
+    /// Should the simplified AST be printed?
+    #[clap(short, long)]
+    simplified: bool,
+}
+
+fn compile(args: &Cli, lib: &HashMap<String, String>) -> Result<(), String> {
+    // Tokenize input.
+    let toks = if args.stdin {
+        let mut src = String::new();
+        std::io::stdin().read_to_string(&mut src).unwrap();
+        let dir = std::env::current_dir().ok();
+        lexer::tokenize(&src, dir.as_ref().map(|s| s.as_path()), lib, None)
+    } else {
+        lexer::tokenize_from_file(&args.path.as_ref().unwrap(), lib, None)
+    }
+    .map_err(|e| format!("Lexer error: {}", e))?;
+    if args.tokens {
+        eprintln!("----------- Tokens -----------");
+        toks.iter().for_each(|(tok, _)| eprint!("{} ", tok));
+        eprintln!("");
+    }
+
+    // Parse the tokens and generate the abstract syntax tree.
+    let ast = parser::parse(toks).map_err(|e| format!("Parser error: {}", e))?;
+    if args.parser {
+        eprintln!("------------ AST -------------");
+        eprintln!("{}", ast);
+        eprintln!("");
+    }
+
+    // Apply intermediate simplifications on the AST:
+    // - replace let statements with function applications
+    // - dedup identifiers
+    // - remove unused variables
+    // - remove identity applications
+    let ast = simplifier::let_remover::remove_lets(ast);
+    let ast = simplifier::unused_remover::remove_unused(ast);
+    let ast = simplifier::id_dedup::dedup_ids(ast);
+    // TODO - remove identity applications
+    if args.simplified {
+        eprintln!("-------- Simplified AST --------");
+        eprintln!("{}", ast);
+        eprintln!("");
+    }
+
+    // Annotate the AST with type information.
+    let ast = annotater::annotate(ast)
+        .map_err(|e| format!("Annotater error: {}", e))
+        .unwrap();
+    if args.annotated {
+        eprintln!("-------- Annotated AST--------");
+        eprintln!("{:#}", ast);
+        eprintln!("");
+    }
+
+    // Simplify the AAST further:
+    // - apply every non tape application.
+    // - move matches so that they are at the root of the function expressions.
+    // - remove matches which are used as arguments to other matches.
+    let ast = simplifier::applier::do_applications(ast);
+    let ast = simplifier::match_mover::move_matches(ast);
+    //let ast = match_merger::merge_matches(ast)?;
+    if args.simplified {
+        eprintln!("-------- Simplified AAST --------");
+        eprintln!("{:#}", ast);
+        eprintln!("");
+    }
+
+    Ok(())
 }
 
 fn main() {
     let args = Cli::parse();
-    // Tokenize input.
-    let toks = lexer::tokenize_from_file(&args.path, &HashMap::new(), None)
-        .map_err(|e| format!("Lexer error: {}", e))
-        .unwrap();
-    println!("----------- Tokens -----------");
-    toks.iter().for_each(|(tok, _)| print!("{} ", tok));
-    println!("");
 
-    // Parse the tokens and generate the abstract syntax tree.
-    println!("------------ AST -------------");
-    let ast = parser::parse(toks)
-        .map_err(|e| format!("Parser error: {}", e))
-        .unwrap();
-    println!("{}", ast);
+    // Load the standard library files.
+    let lib = load_lib!("std/iter.tmc", "std/check.tmc");
 
-    // Check if the AST is valid and annotate it with extra information.
-    println!("-------- Annotated AST--------");
-    let ast = annotater::annotate(ast)
-        .map_err(|e| format!("Annotater error: {}", e))
-        .unwrap();
-    println!("{:#}", ast);
-
-    // Simplify the abstract syntax tree.
-    println!("------- Simplified AST -------");
-    let ast = simplifier::simplify(ast);
-    println!("{:#}", ast);
+    // Compile with the input arguments and the standard library.
+    std::process::exit(if let Err(err) = compile(&args, &lib) {
+        eprintln!("Compilation failed: {}", err);
+        1
+    } else {
+        eprintln!("Compilation successful!");
+        0
+    });
 }
